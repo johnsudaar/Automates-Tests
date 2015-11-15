2077ea97673144e946b1ea1393c52faa3d5fb6e0
Hurter Jonathan <jonathan@hurter.fr>
2015-11-15 20:27:13 +0100

    Comment is life

--- src/AutomateTransformer.java
+++ src/AutomateTransformer.java
@@ -94,6 +94,7 @@ public class AutomateTransformer {
 
     public static Automate completeAndDeterminize(Automate aut){
         Automate a = AutomateTransformer.determinize(aut);
+        // S'il n'est pas complet : Ajout d'un noeud poubelle
         int new_node = -1;
         for(int i = 0; i < a.size(); i++){
             for(char c : a.alphabet()){
@@ -111,10 +112,10 @@ public class AutomateTransformer {
 
     public static Automate minimize(Automate aut) {
 
-        Automate a = AutomateTransformer.completeAndDeterminize(aut);
+        Automate a = AutomateTransformer.completeAndDeterminize(aut); // On part d'un automate complet et deterministe
 
-        ArrayList<Integer> groups = new ArrayList<Integer>();
-        for (int i = 0; i < a.size(); i++) {
+        ArrayList<Integer> groups = new ArrayList<Integer>(); // Permet de stocker le groupe correspondant a un noeud
+        for (int i = 0; i < a.size(); i++) { // Initialisation de la liste des groupes (acceptant => 1, pas acceptant => 0)
             if (a.isAcceptant(i)) {
                 groups.add(i, 1);
             } else {
@@ -122,17 +123,20 @@ public class AutomateTransformer {
             }
         }
 
-        boolean same;
-        ArrayList<Map<Character, Integer>> links;
-        ArrayList<Map<Integer, Map<Character, Integer>>> g_link;
-        ArrayList<Integer> new_groups;
+        boolean same; // Condition d'arret
+        ArrayList<Map<Character, Integer>> links; // Permet de stocker les associations suivantes : Noeuds -> transition -> groupe
+        ArrayList<Map<Integer, Map<Character, Integer>>> g_link; // Permet de stocker les associations suivantes Groupe -> transition -> groupe (pour chacun des anciens groupes)
+        ArrayList<Integer> new_groups; // Variable de travail pour groups
         int nb_groups;
         int old_nb_groups = 2;
         do{
-            nb_groups = 0;
+            nb_groups = 0; // Permet de savoir combien de groupes ont été créé lors de cette itération
+            // Initialisations
             links = new ArrayList<Map<Character, Integer>>();
             g_link = new ArrayList<Map<Integer, Map<Character, Integer>>>();
             new_groups = new ArrayList<Integer>();
+
+            // Recherche du groupe pointé par un charactere depuis tous les noeuds de l'automate
             for (int i = 0; i < a.size(); i++) {
                 links.add(i, new HashMap<Character, Integer>());
                 for (Character c : a.alphabet()) {
@@ -141,19 +145,23 @@ public class AutomateTransformer {
                 }
             }
 
+
+            // Initialisation de g_link : On crée une table d'association groupe -> Transition -> groupe pour chacun des anciens groupes
             for(int i = 0; i < old_nb_groups; i++){
                 g_link.add(i, new HashMap<Integer, Map<Character,Integer>>());
             }
 
+            // Mise a jour de g_link
             for (int i = 0; i < a.size(); i++) {
                 int node_group = -1;
                 int old_node_group = groups.get(i);
+                // Pour chacun des noeuds, nous cherchons si la sequence visée par celui ci existe déjà dans notre branche
                 for(Map.Entry<Integer, Map<Character, Integer>> entry : g_link.get(old_node_group).entrySet()){
                     if(entry.getValue().equals(links.get(i))){
-                        node_group = entry.getKey();
+                        node_group = entry.getKey(); // Si oui on attribue le groupe au noeud trouvé
                     }
                 }
-                if (node_group == -1) {
+                if (node_group == -1) { // Sinon on créé le groupe en fonction de la séquence du noeud
                     node_group = nb_groups;
                     nb_groups++;
                     g_link.get(old_node_group).put(node_group, links.get(i));
@@ -163,7 +171,7 @@ public class AutomateTransformer {
             }
 
             same = true;
-
+            // Calcul de la condition d'arret : On regarde si il y a une différences entre les nouveaux groupes et ceux del'étape N-1
             for (int i = 0; i < a.size() && same; i++) {
                 int n_g = new_groups.get(i);
                 int o_g = new_groups.get(i);
@@ -181,6 +189,7 @@ public class AutomateTransformer {
 
         }while(! same);
 
+        // Une fois finit nous calculons le groupe de chacun des noeuds
         for (int i = 0; i < a.size(); i++) {
             links.add(i, new HashMap<Character, Integer>());
             for (Character c : a.alphabet()) {
@@ -190,7 +199,7 @@ public class AutomateTransformer {
         }
 
         ArrayList<Integer> acc = new ArrayList<Integer>();
-
+        // Récuperation de la liste des groupes acceptants
         for(int i = 0; i< a.size(); i++){
             if(a.isAcceptant(i)){
                 if(! acc.contains(groups.get(i))) {
@@ -200,10 +209,11 @@ public class AutomateTransformer {
         }
 
         int[] ac = Utilities.toIntArray(acc.toArray(new Integer[acc.size()]));
-
+        // Initialisation de l'automate
         Automate r = new Automate(groups.get(a.getInitialNode()),ac , nb_groups, Utilities.toCharArray(a.alphabet()));
         ArrayList<Integer> done = new ArrayList<Integer>();
 
+        // Ajout des liens
         for(int i = 0; i < a.size(); i++){
             int group = groups.get(i);
             if(! done.contains(group)){
@@ -217,142 +227,4 @@ public class AutomateTransformer {
 
         return r;
     }
-
-/*    public static Automate minimize(Automate aut){
-
-        // ---   PHASE 1 : INITIALISATION ---
-        Automate a = AutomateTransformer.completeAndDeterminize(aut);
-
-        // Associe un noeud a un groupe
-        ArrayList<Integer> group = new ArrayList<Integer>();
-
-        // Permet de donner le groupe atteint par un charactere depuis un noeud
-        ArrayList<Map<Character, Integer>> links;
-
-        // Condition d'arret pour la boucle principale (cf etape 4)
-        boolean same = false;
-
-
-
-        // Initialisation des groupes avec les noeuds acceptants
-        for(int i = 0; i < a.size(); i++){
-            if(a.isAcceptant(i)){
-                group.add(i,0);
-            } else {
-                group.add(i,1);
-            }
-        }
-
-        do{
-
-            // --- PHASE 2 : CREATION DE LA LISTE DES NOEUDS ---
-
-            // Permet de lier un noeud a un groupe en passant par un charactere
-            links = new ArrayList<Map<Character, Integer>>();
-
-            // On parcourt la liste des noeuds
-            for(int i = 0; i < a.size(); i++){
-                links.add(i, new HashMap<Character, Integer>());
-                // Pour toutes les lettres de l'alphabet
-                for(char c : a.alphabet()){
-                    // On met ajoute le lien entre le noeud et le groupe
-                    int l = a.exec(i,c).get(0);
-                    links.get(i).put(c, group.get(l));
-                }
-            }
-
-            // --- PHASE 3 : MISE A JOUR DES GROUPES ---
-
-            // Creation d'un tableau temporaire de groupes
-
-            ArrayList<Integer> new_groups = new ArrayList<Integer>();
-            ArrayList<Map<Character, Integer>> signification = new ArrayList<Map<Character, Integer>>();
-
-            // Pour tous les noeuds
-            for(int i = 0 ; i < links.size(); i++){
-                int g = -1;
-                // On cherche si leurs associations ont déjà été trouvée a ce niveau
-                for(int j = 0 ; j < signification.size(); j++){
-                    // Si l'association a été trouvée on stocke le numero du groupe
-                    if(links.get(i).equals(signification.get(j))){
-                        g = j;
-                        break;
-                    }
-                }
-                // Sinon on le créé
-                if( g == -1 ){
-                    g = signification.size();
-                    signification.add(g, links.get(i));
-                }
-                // On met a jour la liste des groupes
-                new_groups.add(i, g);
-            }
-
-            // --- ETAPE 4 : Recherche de differences entre les nouveaux groupes et les anciens
-
-            same = true;
-            // Pour tous les noeuds
-            for(int i = 0; i < group.size() && same; i++) {
-                int g = group.get(i); // Ancien groupe
-                int n_g = new_groups.get(i); // Nouveau groupe
-
-                // On reparcours les autres groupes pour trouver une differece
-                for(int j = 0; j < group.size(); j++){
-                    if(group.get(j) == g && new_groups.get(j) != n_g){ // Si l'on a trouvé un noeud qui était dans le meme groupe avant mais qui ne l'est plus, on  a trouvé une incohérence il faut continuer.
-                        same = false;
-                        break;
-                    }
-                }
-            }
-
-            group = new ArrayList<Integer>(new_groups);
-        }while(! same);
-
-        // ETAPE 5 : Creation du nouvel automate
-
-        int nb_node = 0; // Nombre de noeuds
-
-        for(int g : group) {
-            if(g > nb_node){
-                nb_node = g;
-            }
-        }
-
-        char[] alphabet = new char[a.alphabet().length];
-
-        for(int i = 0; i < a.alphabet().length; i++) {
-            alphabet[i] = a.alphabet()[i];
-        }
-
-        ArrayList<Integer> accepting = new ArrayList<Integer>(); // Acceptants
-
-
-        for(int i = 0; i < a.size(); i++){
-            if(a.isAcceptant(i)){
-                if(! accepting.contains(group.get(i))) {
-                    accepting.add(group.get(i));
-                }
-            }
-        }
-
-        Integer[] acc = accepting.toArray(new Integer[accepting.size()]);
-
-        int initial = group.get(a.getInitialNode());
-
-        Automate n = new Automate(initial, Utilities.toIntArray(acc), nb_node, alphabet);
-
-        ArrayList<Integer> done = new ArrayList<Integer>();
-
-        for(int i = 0; i < links.size(); i++){
-            int g = group.get(i);
-            if(! done.contains(i)){
-                done.add(i);
-                for(Map.Entry<Character, Integer> entry : links.get(0).entrySet()){
-                    n.lier(i,entry.getValue(),entry.getKey());
-                }
-            }
-        }
-
-        return n;
-    }*/
 }
